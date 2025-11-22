a03d7218da22f3bd1f22f2f975bb225c90cf9494
Dmitrii Kotik <dmdoggx@gmail.com>
2025-11-22 16:02:54 +0700

    feat: CORS added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware # <-- Добавьте эту строку
 
 from app.api.routes.link import router as link_router
 
@@ -7,6 +8,21 @@ app = FastAPI(
     description='URL Shortener',
     version='0.1.0'
 )
+
+origins = [
+    "https://www.simplelinker.cc",
+    "https://simplelinker.cc",
+    "https://simplelinker-frontend.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(link_router, prefix='/api')
 

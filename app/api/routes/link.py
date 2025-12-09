119217bd73d2cc40ae8b06fbd39f77839b5a8e1e
Dmitrii Kotik <dmdoggx@gmail.com>
2025-12-09 17:11:10 +0700

    small refactoring

--- app/api/routes/link.py
+++ app/api/routes/link.py
@@ -8,15 +8,21 @@ from app.core.services.link import LinkService
 router = APIRouter()
 
 @router.post('/', response_model=ShortLinkResponse, status_code=201)
-async def create_short_link(data: CreateShortLink,
-                            link_service: LinkService = Depends(get_link_service)):
+async def create_short_link(
+        data: CreateShortLink,
+        link_service: LinkService = Depends(get_link_service)
+):
     short = await link_service.generate_link(data)
-    return ShortLinkResponse(link=data.link,
-                             link_with_slug=short)
+    return ShortLinkResponse(
+        link=data.link,
+        link_with_slug=short
+    )
 
 @router.get('/{slug}', status_code=307)
-async def go_to_link(slug: str,
-                     link_service: LinkService = Depends(get_link_service)):
+async def go_to_link(
+        slug: str,
+        link_service: LinkService = Depends(get_link_service)
+):
     original_url = await link_service.get_link_by_slug(slug)
     return RedirectResponse(url=original_url)
 
